80f5bec79955e38e6bfc744338ad7a33924b83d0
Andrew Wiens <andywiens@Andrews-MacBook-Pro.local>
2018-02-15 17:27:27 -0800

    added friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,7 +14,22 @@ var friends = [{
         5,
         5,
     ]
-}];
+}, {
+        name: "Carrot Top",
+        photo: "https://s1.ticketm.net/tm/en-us/dam/a/857/46edb661-db67-42f1-81f3-66f9d5f02857_289581_CUSTOM.jpg",
+        selection: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+        ]
+    }];
 
 // Export Array
 module.exports = friends;
